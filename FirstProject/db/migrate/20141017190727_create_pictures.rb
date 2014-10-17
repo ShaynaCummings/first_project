bb1f575e4c7a067146ad3713fae1c6b6afd033f7
shaynaCummings <shayna.cummings@gmail.com>
2014-10-17 15:18:23 -0400

    set up models and controllers

---
+++ FirstProject/db/migrate/20141017190727_create_pictures.rb
@@ -0,0 +1,13 @@
+class CreatePictures < ActiveRecord::Migration
+
+  def change
+    create_table :pictures do |t|
+      t.string :url
+      t.integer :times_saved
+      t.integer :times_skipped
+      t.belongs_to :user
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ FirstProject/db/migrate/20141017190857_create_tags.rb
@@ -0,0 +1,11 @@
+class CreateTags < ActiveRecord::Migration
+
+  def change
+    create_table :tags do |t|
+      t.string :tag_name
+      t.belongs_to :picture
+
+      t.timestamps null: false
+    end
+  end
+end
